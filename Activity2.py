4cfbeb4292062fbaded0cc743fc9021fca4d1b1e
Byr3n <sealunion45@gmail.com>
2025-02-15 18:13:54 -0800

    first commit

---
+++ Activity2.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a whole number numeriator "))
+numd=int(input("Enter a whole number a denominator "))
+if num%numd==0:
+    print(str(num)+" is divisible by "+str(numd))
+else:
+    print(str(num)+" they're not divisible "+ str(numd))
---
+++ Activity3.py
@@ -0,0 +1,10 @@
+mean=38
+wrong_num=36
+correct_num=56
+total_num=40
+sum=total_num*mean
+print("the sum of 40 is  ",sum)
+sum2=(sum-wrong_num)+correct_num
+print("sum-wrong_num+correct_num=" ,sum2)
+mean2=sum2/total_num
+print(mean2)
